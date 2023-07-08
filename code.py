bf9c3a2bd646754e7cabddc28ca09afaec6cb001
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-07-08 16:20:36 -0300

    code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 import paramiko
 import os
+from ftplib import FTP
 
 def conectar_ssh():
     host = input("Digite o endereço do servidor SSH: ")
@@ -55,26 +56,83 @@ def conectar_netcat():
         print(f"Erro ao conectar com o servidor Netcat: {str(e)}")
 
 def abrir_vnc():
-    porta = input("Digite a porta para abrir o servidor VNC: ")
+    opcao = input("Digite 1 para conectar a um servidor VNC existente ou 2 para abrir um novo servidor VNC: ")
 
-    try:
-        os.system(f"vncserver :{porta}")
-        print(f"Servidor VNC aberto na porta {porta}. Aguardando conexões...")
-    except Exception as e:
-        print(f"Erro ao abrir servidor VNC: {str(e)}")
+    if opcao == "1":
+        host = input("Digite o endereço do servidor VNC: ")
+        porta = input("Digite a porta do servidor VNC: ")
+
+        try:
+            os.system(f"vncviewer {host}:{porta}")
+            print(f"Conectado ao servidor VNC {host}:{porta}")
+        except Exception as e:
+            print(f"Erro ao conectar ao servidor VNC: {str(e)}")
+
+    elif opcao == "2":
+        porta = input("Digite a porta para abrir o servidor VNC: ")
+
+        try:
+            os.system(f"vncserver :{porta}")
+            print(f"Servidor VNC aberto na porta {porta}. Aguardando conexões...")
+        except Exception as e:
+            print(f"Erro ao abrir servidor VNC: {str(e)}")
+
+    else:
+        print("Opção inválida. Tente novamente.")
+
+def conectar_ftp():
+    opcao = input("Digite 1 para conectar a um servidor FTP existente ou 2 para abrir um novo servidor FTP: ")
+
+    if opcao == "1":
+        host = input("Digite o endereço do servidor FTP: ")
+        username = input("Digite o nome de usuário: ")
+        password = input("Digite a senha: ")
+
+        try:
+            ftp = FTP(host)
+            ftp.login(username, password)
+            print("Conexão FTP estabelecida com sucesso!")
+            # Faça outras operações ou comandos FTP aqui
+
+            ftp.quit()
+
+        except Exception as e:
+            print(f"Erro ao estabelecer conexão FTP: {str(e)}")
+
+    elif opcao == "2":
+        porta = input("Digite a porta para abrir o servidor FTP: ")
+        username = input("Digite o nome de usuário: ")
+        password = input("Digite a senha: ")
+
+        try:
+            server = FTP()
+            server.set_debuglevel(2)
+            server.bind(('', int(porta)))
+            server.login(username, password)
+            print(f"Servidor FTP aberto na porta {porta}. Aguardando conexões...")
+            # Faça outras operações ou tratamentos de conexões aqui
+
+            server.close()
+
+        except Exception as e:
+            print(f"Erro ao abrir servidor FTP: {str(e)}")
+
+    else:
+        print("Opção inválida. Tente novamente.")
 
 def exibir_menu():
     print("========== MENU DE CONEXÃO ==========")
     print("1. Conectar a um servidor SSH")
     print("2. Abrir um servidor SSH")
     print("3. Conectar usando Netcat")
-    print("4. Abrir um servidor VNC")
-    print("5. Sair")
+    print("4. Abrir ou conectar a um servidor VNC")
+    print("5. Abrir ou conectar a um servidor FTP")
+    print("6. Sair")
 
 def main():
     while True:
         exibir_menu()
-        opcao = input("Selecione uma opção (1-5): ")
+        opcao = input("Selecione uma opção (1-6): ")
 
         if opcao == "1":
             conectar_ssh()
@@ -85,6 +143,8 @@ def main():
         elif opcao == "4":
             abrir_vnc()
         elif opcao == "5":
+            conectar_ftp()
+        elif opcao == "6":
             print("Encerrando o programa...")
             break
         else:
